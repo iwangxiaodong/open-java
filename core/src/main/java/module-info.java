c77b4f4b40912a7e13eef85ce19e681b0e96b6b5
xiaodong <i@wangxiaodong.com>
2018-04-17 19:05:27 +0800

    .

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.openle.module.core {
     requires java.logging;
     requires jdk.compiler;
     exports com.openle.module.core;
+    exports com.openle.module.core.io;
     exports com.openle.module.core.converter;
     exports com.openle.module.core.lambda;
 }
