003bf8ad6fe08365b758868d278c0997bf0dd064
168 <168@DESKTOP-I7GHIVP>
2019-11-14 17:16:19 +0800

    add OurPEM

---
+++ core/src/main/java/com/openle/our/core/security/OurPEM.java
@@ -0,0 +1,38 @@
+package com.openle.our.core.security;
+
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+/**
+ * 无问题后取代PemUtils
+ *
+ * @author xiaodong
+ */
+//  或用bcprov-jdk15on的PemReader类
+public class OurPEM {
+
+    public static String pemPublicKeyToBase64(String pemPublicKeyString) {
+        if (pemPublicKeyString != null && pemPublicKeyString.trim().length() > 0
+                && pemPublicKeyString.contains("BEGIN PUBLIC KEY")
+                && pemPublicKeyString.contains("END PUBLIC KEY")) {
+            pemPublicKeyString = pemPublicKeyString.trim().replaceAll("-----BEGIN PUBLIC KEY-----", "")
+                    .replaceAll("-----END PUBLIC KEY-----", "").replaceAll("\n", "").trim();
+            return pemPublicKeyString;
+        }
+        return null;
+    }
+
+    public static PublicKey getPublicKeyByBase64PublicKey(String base64PublicKey) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        byte[] keyBytes;
+        keyBytes = Base64.getDecoder().decode(base64PublicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        return publicKey;
+    }
+
+}
