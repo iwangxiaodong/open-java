e754f8a5aadb74fdb1364552a8399e429bb28272
168 <168@DESKTOP-I7GHIVP>
2019-11-27 13:26:29 +0800

    AbstractSpecification added lazyExpression

--- core/src/main/java/com/openle/our/core/specification/AbstractSpecification.java
+++ core/src/main/java/com/openle/our/core/specification/AbstractSpecification.java
@@ -1,6 +1,5 @@
 package com.openle.our.core.specification;
 
-@SuppressWarnings("unchecked")
 public abstract class AbstractSpecification<T> implements Specification<T> {
 
     @Override
@@ -19,5 +18,23 @@ public abstract class AbstractSpecification<T> implements Specification<T> {
     @Override
     public Specification<T> not(final Specification<T> specification) {
         return new NotSpecification<>(specification);
+    }
+
+    /*
+    
+    可选 - 懒加载避免全数据遍历
+    
+    //  未采用CQRS的资源库可通过规约列出聚合
+    public abstract Optional<List<T>> listBySpecification(Specification specification);
+
+     */
+    private Object lazyExpression;
+
+    protected void setLazyExpression(Object lazyExpression) {
+        this.lazyExpression = lazyExpression;
+    }
+
+    public Object lazyExpression() {
+        return this.lazyExpression;
     }
 }
--- core/src/main/java/com/openle/our/core/specification/AndSpecification.java
+++ core/src/main/java/com/openle/our/core/specification/AndSpecification.java
@@ -1,31 +1,18 @@
 package com.openle.our.core.specification;
 
-/**
- * AND specification, used to create a new specifcation that is the AND of two other specifications.
- */
-@SuppressWarnings("unchecked")
 public class AndSpecification<T> extends AbstractSpecification<T> {
 
-  private Specification<T> spec1;
-  private Specification<T> spec2;
+    private final Specification<T> spec1;
+    private final Specification<T> spec2;
 
-  /**
-   * Create a new AND specification based on two other spec.
-   *
-   * @param spec1 Specification one.
-   * @param spec2 Specification two.
-   */
-  public AndSpecification(final Specification<T> spec1, final Specification<T> spec2) {
-    this.spec1 = spec1;
-    this.spec2 = spec2;
-  }
+    public AndSpecification(final Specification<T> spec1, final Specification<T> spec2) {
+        this.spec1 = spec1;
+        this.spec2 = spec2;
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isSatisfiedBy(final T t) {
-    return spec1.isSatisfiedBy(t) && spec2.isSatisfiedBy(t);
-  }
-  
+    @Override
+    public boolean isSatisfiedBy(final T t) {
+        return spec1.isSatisfiedBy(t) && spec2.isSatisfiedBy(t);
+    }
+
 }
